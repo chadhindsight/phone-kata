9786ab1364c291ed5f3b2d26b29aadf9e827c4be
Chad Hinds <chadrickj8@gmail.com>
2020-01-11 20:35:44 -0500

    change middle digit

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,6 @@ function createPhoneNumber(numbers) {
 
     return `(${numbers.substring(0, 3)}) ${numbers.substring(3, 6)}-${numbers.substring(6)}`
 }
+
+// TEST
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 5, 9, 0]));
